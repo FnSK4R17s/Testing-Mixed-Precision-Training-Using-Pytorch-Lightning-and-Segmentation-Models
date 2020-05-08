9020b3373bc2d02ab92d52e9d3f008c353925255
FnSK4R17s <convey.sk4r@gmail.com>
2020-05-08 21:36:51 +0530

    Completed training

--- src/Lightning_trainer.py
+++ src/Lightning_trainer.py
@@ -16,8 +16,8 @@ def train_iterative(train_folds, val_folds):
 
 if __name__ == "__main__":
 
-    # train_iterative([4, 1, 2, 3],[0]) # 02:00:00
-    # train_iterative([0, 4, 2, 3],[1]) # 04:00:00
+    train_iterative([4, 1, 2, 3],[0]) # 02:00:00
+    train_iterative([0, 4, 2, 3],[1]) # 04:00:00
     train_iterative([0, 1, 4, 3],[2]) # 06:00:00
-    # train_iterative([0, 1, 2, 4],[3]) # 08:00:00
-    # train_iterative([0, 1, 2, 3],[4]) # 10:00:00
+    train_iterative([0, 1, 2, 4],[3]) # 08:00:00
+    train_iterative([0, 1, 2, 3],[4]) # 10:00:00
